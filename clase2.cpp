5c9a0e69d9f94c781d6719c0bdb93fba4dcc1cf9
IsabellaMuroni <isa_sk_@hotmail.com>
2019-03-19 16:25:02 -0300

    2do commit

--- clase2.cpp
+++ clase2.cpp
@@ -5,9 +5,15 @@ void main()
 {
 	int Buff[5] = (5,15,30,10,35);
 	int *p;
+	int i;
 	p = Buff;
 
 	//Acceder a los elementos mediante notación subindexada
-	hghghgh
 
+	for (i = 0; i < 5; ++i)
+	{
+		printf("%d\n", Buff[i]);
+	}
+	//Acceda a los elementos del array mediante el puntero p on aritmetica de punteros
+	return 0;
 }

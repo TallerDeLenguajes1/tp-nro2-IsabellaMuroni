c53562757035d6918023fa88c16357153cdcf151
IsabellaMuroni <isa_sk_@hotmail.com>
2019-03-21 19:34:22 -0300

    Matiz cargada

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -1,16 +1,34 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 #define N  4
 #define M  5
 
-
+int main()
+{
 	int f,c;
 	double mt [N] [M];
+	//double num;
+	//num = rand() % (15-5+1);
 
 	for (f = 0; f < N; ++f)
 	{
 		for (c = 0; c < M; ++c)
 		{
-			cprintf("%lf  ", mt [f] [c]);
+			mt [f] [c]  = rand() % (15-5+1);
 		}
-		cprintf("\n");
+		printf("\n");
 	}
+
+
+	for (f = 0; f < N; ++f)
+	{
+		for (c = 0; c < M; ++c)
+		{
+			printf("%lf  ", mt [f] [c]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+	
